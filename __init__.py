b0465b8545e88bc4fb86dce15093420885f2ece0
warranty99 <146296228+warranty99@users.noreply.github.com>
2023-12-29 15:41:23 -0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import subprocess
+
+Install dependencies using pip
+subprocess.call(["pip", "install", "elevenlabs"])
+subprocess.call(["pip", "install", "openai"])
+subprocess.call(["pip", "install", "numpy"])
+subprocess.call(["pip", "install", "sounddevice"])
+
+
