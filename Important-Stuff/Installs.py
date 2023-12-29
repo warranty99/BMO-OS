fa98dcd7313f6ce1c3f66fd26bca4f230e13a67f
warranty99 <146296228+warranty99@users.noreply.github.com>
2023-12-29 15:38:29 -0300

    Delete Installs.py

--- Important-Stuff/Installs.py
+++
@@ -1,6 +0,0 @@
-subprocess.call(["pip", "install", "elevenlabs"])
-subprocess.call(["pip", "install", "openai"])
-subprocess.call(["pip", "install", "numpy"])
-subprocess.call(["pip", "install", "sounddevice"])
-
-#awesome
