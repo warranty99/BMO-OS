1a2b96038c7ba6c02c754d111da94144295e6f59
warranty99 <146296228+warranty99@users.noreply.github.com>
2023-12-29 15:38:55 -0300

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='your-package-name',
+    version='0.1',
+    packages=['your_package'],
+    install_requires=[
+        'elevenlabs',
+        'openai',
+        'numpy',
+        'sounddevice',
+        #For now..
+    ],
+)
