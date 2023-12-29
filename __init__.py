88b196bb5e11c68e182deabf774370bc8f2646ac
warranty99 <146296228+warranty99@users.noreply.github.com>
2023-12-29 15:41:53 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import subprocess
 
-Install dependencies using pip
 subprocess.call(["pip", "install", "elevenlabs"])
 subprocess.call(["pip", "install", "openai"])
 subprocess.call(["pip", "install", "numpy"])
